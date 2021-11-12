c70799506b22973be9b5b442da5ae112e1195cbf
the-breanne <77354893+the-breanne@users.noreply.github.com>
2021-11-12 12:46:41 -0600

    Update Auth.vue

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -82,7 +82,6 @@
         username: [
           v => !!v || "Username is required",
           v => (v && v.length > 3) || "A username must be more than 3 characters long",
-          v => /^[a-z0-9_]+$/.test(v) || "A username can only contain letters and digits"
         ],
         password: [
           v => !!v || "Password is required",
