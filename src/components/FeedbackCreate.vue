bc6803b9127e369a23d4dfa42112a5be4ebcee7f
Breanne Beutler <breannebeutler@unomaha.edu>
2022-03-21 10:15:52 -0500

    Update feedback

---
+++ src/components/FeedbackCreate.vue
@@ -0,0 +1,194 @@
+<template>
+  <v-container grid-list-md>
+    <v-row align="center" justify="center">
+      <v-col class="align-center">
+        <v-row class="align-center" justify="center">
+          <v-col cols="12" sm="10" md="10" lg="6" class="align-center">
+            <v-alert v-if="showMsg === 'error'"
+                     dismissible
+                     :value="true"
+                     type="error"
+            >
+              Please verify Feedback information.
+            </v-alert>
+          </v-col>
+        </v-row>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="10" md="10" lg="6" class="align-center">
+            <v-card class="login-card">
+              <v-card-title>
+                <span class="headline">{{pageTitle}}</span>
+              </v-card-title>
+              <v-spacer/>
+
+              <v-card-text>
+                <v-form ref="form" lazy-validation>
+                  <v-container>
+
+                    <!--v-text-field
+                    v-model="feedback.cust_number"
+                    label="Employee"
+                    required
+                    type="number"
+                    /-->
+                    <v-select
+                    v-model="feedback.employee"
+                    label="Employee Number"
+                    :items="list"
+                    item-value='pk'
+                    item-text='employee_number'
+                    ></v-select>
+
+                    <v-select
+                    v-model="feedback.manager"
+                    label="Manager Number"
+                    :items="list"
+                    item-value='pk'
+                    item-text='manager_number'
+                    ></v-select>
+
+                    <v-text-field
+                    v-model="feedback.subject"
+                    label="Subject"
+                    required
+                    />
+                    <v-text-field
+                    v-model="feedback.comment"
+                    label="Comment"
+                    required
+                    />
+
+                    <v-text-field
+                    v-model="feedback.requested_date"
+                    label="Requested Date"
+                    required
+                    type="date"
+
+                    />
+
+                    <v-text-field
+                    v-model="feedback.created_date"
+                    label="Created Date"
+                    required
+                    type="date"
+                    />
+
+                </v-container>
+                <v-btn v-if="!isUpdate" class="blue white--text" @click="createFeedback">Save</v-btn>
+                <v-btn v-if="isUpdate" class="blue white--text" @click="updateFeedback">Update</v-btn>
+                <v-btn class="white black--text" @click="cancelOperation">Cancel</v-btn>
+                </v-form>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+
+<script>
+  import router from '../router';
+  import {APIService} from '../http/APIService';
+  const apiService = new APIService();
+
+  export default {
+    name: 'FeedbackCreate',
+    components: {},
+    data() {
+      return {
+        employees: [],
+        showError: false,
+        feedback: {},
+        pageTitle: "Add New Feedback",
+        isUpdate: false,
+        showMsg: '',
+      };
+    },
+    computed:{
+      list:{
+      get () {
+            return this.employees
+        },
+          set (newValue) {
+            this.employees = newValue
+          }
+      }
+    },
+    methods: {
+      getEmployees() {
+        apiService.getEmployeeList().then(response => {
+          this.employees = response.data.data;
+          if (localStorage.getItem("isAuthenticates")
+            && JSON.parse(localStorage.getItem("isAuthenticates")) === true) {
+            this.validUserName = JSON.parse(localStorage.getItem("log_user"));
+          }
+        }).catch(error => {
+          if (error.response.status === 401) {
+            localStorage.removeItem('isAuthenticates');
+            localStorage.removeItem('log_user');
+            localStorage.removeItem('token');
+            router.push("/auth");
+          }
+        });
+      },
+      createFeedback() {
+        apiService.addNewFeedback(this.feedback).then(response => {
+          if (response.status === 201) {
+            this.feedback = response.data;
+             this.showMsg = "";
+            router.push('/feedback-list/new');
+          }else{
+              this.showMsg = "error";
+          }
+        }).catch(error => {
+          if (error.response.status === 401) {
+            router.push("/auth");
+          }else if (error.response.status === 400) {
+            this.showMsg = "error";
+          }
+        });
+      },
+      cancelOperation(){
+         router.push("/feedback-list");
+      },
+      updateFeedback() {
+        apiService.updateFeedback(this.feedback).then(response => {
+          if (response.status === 200) {
+            this.feedback = response.data;
+            router.push('/feedback-list/update');
+          }else{
+              this.showMsg = "error";
+          }
+        }).catch(error => {
+          if (error.response.status === 401) {
+            router.push("/auth");
+          }else if (error.response.status === 400) {
+            this.showMsg = "error";
+          }
+        });
+      }
+    },
+    mounted() {
+      this.getEmployees();
+      if (this.$route.params.pk) {
+        this.pageTitle = "Edit Feedback";
+        this.isUpdate = true;
+        apiService.getFeedback(this.$route.params.pk).then(response => {
+          this.feedback = response.data;
+        }).catch(error => {
+          if (error.response.status === 401) {
+            router.push("/auth");
+          }
+        });
+      }
+    },
+  }
+</script>
+<style scoped>
+  .aform {
+    margin-left: auto;
+    width: 60%;
+  }
+</style>
---
+++ src/components/FeedbackList.vue
@@ -0,0 +1,197 @@
+<template>
+  <v-main>
+    <!-- Welcome title -->
+    <v-container fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="12" align="center" justify="center">
+          <blockquote>
+            Welcome {{validUserName}}!
+            <footer>
+              <small>
+                <em>Kudos</em>
+              </small>
+            </footer>
+          </blockquote>
+        </v-col>
+        <v-col  cols="12" md="10" lg="10" align="center" justify="center">
+          <v-alert v-if="showMsg === 'new'"
+                   dismissible
+                   :value="true"
+                   type="success"
+          >
+            New feedback has been added.
+          </v-alert>
+          <v-alert v-if="showMsg === 'update'" dismissible
+                   :value="true"
+                   type="success"
+          >
+           Feedback information has been updated.
+          </v-alert>
+          <v-alert v-if="showMsg === 'deleted'" dismissible
+                   :value="true"
+                   type="success"
+          >
+            Selected Feedback has been deleted.
+          </v-alert>
+        </v-col>
+      </v-row>
+
+      <!-- Data table -->
+      <v-row align="center" justify="center">
+        <v-col cols="12" md="10" v-resize="onResize">
+            <v-data-table
+              :headers="headers"
+              :items="feedbacks"
+              class="elevation-1"
+              style="max-height: 300px; overflow-y: auto"
+              v-if="!isMobile"
+            >
+                    <template v-slot:item="props">
+                      <tr>
+                        <td align="left">{{ props.item.employee_number }}</td>
+                        <td align="left">{{ props.item.manager_number }}</td>
+                        <td align="left">{{ props.item.subject }}</td>
+                        <td nowrap="true" align="left">{{ props.item.comment }}</td>
+                        <td nowrap="true" align="left">{{ props.item.requested_date }}</td>
+                        <td nowrap="true" align="left">{{ props.item.created_date }}</td>
+                        <td align="center"><v-icon @click="updateFeedback(props.item)">mdi-pencil</v-icon></td>
+                        <td align="center"><v-icon @click="deleteFeedback(props.item)">mdi-delete</v-icon></td>
+                      </tr>
+                    </template>
+              </v-data-table>
+              <v-data-iterator
+                :items="feedbacks"
+                hide-default-footer
+                v-else
+              >
+                <template v-slot:default="{ items, isExpanded, expand }">
+                  <v-row>
+                    <v-col
+                      v-for="item in items"
+                      :key="item.name"
+                      cols="12"
+                    >
+                      <v-card>
+                        <v-card-title class="pb-0 pt-0 pl-0">
+                          <v-col cols="9" class="text-left body-2 text-truncate">{{item.employee_number}} - {{item.category }}</v-col>
+                          <v-col cols="3" class="text-center">
+                            <v-card-actions>
+                              <v-icon @click="updateFeedback(item)" class="small">mdi-pencil</v-icon>
+                              <v-icon @click="deleteFeedback(item)" class="small">mdi-delete</v-icon>
+                              <v-icon @click.native="expand(item, !isExpanded(item))" class="small">mdi-dots-horizontal</v-icon>
+                            </v-card-actions>
+                          </v-col>
+                        </v-card-title>
+                        <v-divider></v-divider>
+
+                        <v-list v-show="isExpanded(item)" dense>
+                          <v-list-item>
+                            <v-list-item-content class="align-end">{{ item.description }}</v-list-item-content>
+                          </v-list-item>
+                
+              
+              
+                        </v-list>
+                      </v-card>
+                    </v-col>
+                  </v-row>
+                </template>
+              </v-data-iterator>
+              <v-btn class="blue mt-4 white--text" @click="addNewFeedback">Add Feedback</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
+</template>
+
+
+<script>
+
+  import router from '../router';
+  import {APIService} from '../http/APIService';
+  const apiService = new APIService();
+
+  export default {
+    name: "FeedbackList",
+    data: () => ({
+      feedbacks: [],
+      validUserName: "Guest",
+      feedbackSize: 0,
+      showMsg: '',
+      isMobile: false,
+      headers: [
+        {text: 'Employee Number', sortable: false, align: 'left',},
+        {text: 'Manager Number', sortable: false, align: 'left',},
+        {text: 'Subject', sortable: false, align: 'left',},
+        {text: 'Comment', sortable: false, align: 'left',},
+        {text: 'Requested_Date', sortable: false, align: 'left',},
+        {text: 'Created_Date', sortable: false, align: 'left',},
+        {text: 'Update', sortable: false, align: 'left',},
+        {text: 'Delete', sortable: false, align: 'left',}
+
+      ],
+
+    }),
+    mounted() {
+      this.getFeedbacks();
+      this.showMessages();
+    },
+    methods: {
+      onResize() {
+          if (window.innerWidth < 600)
+            this.isMobile = true;
+          else
+            this.isMobile = false;
+        },
+      showMessages(){
+        console.log(this.$route.params.msg)
+        if (this.$route.params.msg) {
+          this.showMsg = this.$route.params.msg;
+        }
+      },
+      getFeedbacks() {
+        apiService.getFeedbackList().then(response => {
+          this.feedbacks = response.data.data;
+          this.feedbackSize = this.feedbacks.length;
+          if (localStorage.getItem("isAuthenticates")
+            && JSON.parse(localStorage.getItem("isAuthenticates")) === true) {
+            this.validUserName = JSON.parse(localStorage.getItem("log_user"));
+          }
+        }).catch(error => {
+          if (error.response.status === 401) {
+            localStorage.removeItem('isAuthenticates');
+            localStorage.removeItem('log_user');
+            localStorage.removeItem('token');
+            router.push("/auth");
+          }
+        });
+      },
+      addNewFeedback() {
+        if (localStorage.getItem("isAuthenticates")
+          && JSON.parse(localStorage.getItem("isAuthenticates")) === true) {
+          router.push('/feedback-create');
+        } else {
+          router.push("/auth");
+        }
+      },
+      updateFeedback(feedback) {
+        router.push('/feedback-create/' + feedback.pk);
+      },
+      deleteFeedback(feedback) {
+        apiService.deleteFeedback(feedback.pk).then(response => {
+          if (response.status === 204) {
+            router.push('/feedback-list/deleted/')
+            this.getFeedbacks()
+          }
+        }).catch(error => {
+          if (error.response.status === 401) {
+            localStorage.removeItem('isAuthenticates');
+            localStorage.removeItem('log_user');
+            localStorage.removeItem('token');
+            router.push("/auth");
+          }
+        });
+      }
+    }
+  };
+</script>
