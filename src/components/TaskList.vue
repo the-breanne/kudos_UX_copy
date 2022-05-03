9c9669fddac85f2fb378ce1507b3966c119ca7b0
Breanne Beutler <breannebeutler@unomaha.edu>
2022-05-03 13:29:23 -0500

    Update TaskList.vue

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -6,7 +6,7 @@
         <v-col cols="12" align="center" justify="center">
           <h2>Task List</h2>
         
-          <h4>This is your task list. Here you can view your tasks and add new tasks to your list.</h3>
+          <h3>This is your task list. Here you can view your tasks and add new tasks to your list.</h3>
         </v-col>
         <v-col  cols="12" md="10" lg="10" align="center" justify="center">
           <v-alert v-if="showMsg === 'new'"
